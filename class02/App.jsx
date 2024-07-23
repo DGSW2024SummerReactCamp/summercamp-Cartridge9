f8e15a47a5350616b409841cedd94f540b5c377a
Cartridge0923 <pjmin0923@gmail.com>
2024-07-23 15:14:07 +0900

    Class02

---
+++ class02/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './App.css';
+import Viewer from './Viewer'; // Viewer.jsx import
+import Counter from './Counter'; // Counter.jsx import
+
+const App = () => {
+  const [count, setCount] = useState(0); // Viewer에 표시하기 위해 useState 생성
+  return (
+    <div>
+      <h2>Simple Counter</h2> {/* 제목 */}
+      <Viewer cnt={count} /> {/* Viewer의 cnt에 count를 전달 */}
+      <Counter cnt={setCount}/> {/* Counter의 cnt에 count를 전달 */}
+    </div>
+  );
+}
+
+export default App;
